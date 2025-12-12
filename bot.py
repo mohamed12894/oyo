9a78ea228b1d7d7ab6f8863ba3ba1ed187eab178
mohamed12894 <professeurcommerce1@gmail.com>
2025-12-12 13:17:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 import os
 
 TOKEN = os.getenv("7976474395:AAHY5dp6MO8BAgonWGIJyDtf0uemUCl4YEA")
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(7976474395:AAHY5dp6MO8BAgonWGIJyDtf0uemUCl4YEA)
 
 @bot.message_handler(commands=['start'])
 def start(msg):
@@ -13,4 +13,5 @@ def reply_all(msg):
     bot.reply_to(msg, "مرحبا! البوت شغّال!")
 
 bot.infinity_polling(skip_pending=True)
+
 
